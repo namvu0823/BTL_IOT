6bf05a62812a6d2c91fda7eca751f56183b7a29a
namvu0823 <vuvannamb1@gmail.com>
2024-12-01 15:31:52 +0700

    update

--- BTL_IOT.ino
+++ BTL_IOT.ino
@@ -1,6 +1,8 @@
+#include <WebSocketsClient.h>
 #include <SPI.h>
-#include <MFRC522.h>
+#include <Adafruit_PN532.h>
 
+#include <Wire.h>
 #include <Keypad.h>
 
 #include <Adafruit_Fingerprint.h>
@@ -30,9 +32,17 @@
 #define I2C_SCL	 22
 #define LED 15
 
+const char* websockets_server_host = ""; // Địa chỉ IP server
+const uint16_t websockets_server_port = 3000; // Cổng server
+
 const char* ssid     = ""; // CHANGE HERE
 const char* password = ""; // CHANGE HERE
 LiquidCrystal_I2C lcd(0x27,16,2);
+HardwareSerial FP(AS_RXD,AS_TXD);
+Adafruit_Fingerprint finger=Adafruit_Fingerprint(&FP);//FINGERPRINT
+WebSocketsClient client;
+Adafruit_PN532 nfc(RC_SDA);//RFID
+uint8_t id;
 
 char  keys[4][4]={
   {'1', '2', '3', 'A'},
@@ -43,6 +53,7 @@ char  keys[4][4]={
 
 byte pin_rows[4]={R1,R2,R3,R4};
 byte pin_column[4]={C1,C2,C3,C4};
+Keypad keypad =Keypad(makeKeymap(keys),pin_rows, pin_column, 4, 4);
 
 esp_err_t init_wifi() {
     WiFi.begin(ssid, password);
@@ -66,25 +77,184 @@ esp_err_t init_wifi() {
     return ESP_OK;
 }
 
-Keypad keypad =Keypad(makeKeymap(keys),pin_rows, pin_column, 4, 4);
+//giao tiếp server
+void onMessageCallback(WebsocketsMessage message) {
+    String msg = message.data();
+    if (msg.startsWith("{")) {
+        // Đây là JSON, parse command
+        // 
+    }
+}
 
+//xử lý websocket
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.println("WebSocket disconnected");
+      break;
+    case WStype_CONNECTED:
+      Serial.println("WebSocket connected");
+      client.sendTXT("ESP32");
+      break;
+    case WStype_TEXT:
+      onMessageCallback(WebsocketsMessage(String((char*)payload)));
+      break;
+  }
+}
 
+//khởi động cảm biến vân tay
+void initFinger(){
+
+  FP.begin(57600);
+  if (finger.begin()) {
+    Serial.println("Cảm biến vân tay đã được khởi tạo thành công!");
+  } else {
+    Serial.println("Không thể khởi tạo cảm biến vân tay. Kiểm tra kết nối.");
+  }
+  uint8_t version = finger.getVersion();
+  if (version == FINGERPRINT_OK) {
+    Serial.print("Phiên bản cảm biến: ");
+    Serial.println(version);
+  } else {
+    Serial.print("Lỗi khi lấy phiên bản: ");
+    Serial.println(version);
+  }
+}
+
+void createFingerprint() {
+  Serial.println("Đặt ngón tay lên cảm biến");
+  
+  // Đọc hình ảnh vân tay
+  int result = finger.getImage();
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Lỗi khi đọc vân tay: ");
+    Serial.println(result);
+    return;
+  }
+
+  // Chuyển đổi hình ảnh thành dữ liệu vân tay
+  result = finger.image2Tz();
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Không thể tạo dữ liệu vân tay: ");
+    Serial.println(result);
+    return;
+  }
+
+  Serial.println("Bỏ tay khỏi cảm biến...");
+  delay(2000); 
+
+  Serial.println("Đặt lại ngón tay lên cảm biến...");
+  
+  // Đọc hình ảnh vân tay lần thứ hai
+  result = finger.getImage();
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Lỗi khi đọc vân tay: ");
+    Serial.println(result);
+    return;
+  }
+
+  // Chuyển đổi hình ảnh lần thứ hai thành dữ liệu vân tay
+  result = finger.image2Tz(1);
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Không thể tạo dữ liệu vân tay: ");
+    Serial.println(result);
+    return;
+  }
+
+  // Tạo mô hình vân tay
+  result = finger.createModel();
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Failed to create model: ");
+    Serial.println(result);
+    return;
+  }
+
+  //  Lưu mô hình vân tay vào bộ nhớ
+  result = finger.storeModel(id); // id vân tay
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Failed to store model: ");
+    Serial.println(result);
+    return;
+  }
+  Serial.println("Tạo thành công vân tay");
+}
+
+void verifyFingerprint() {
+  Serial.println("Đạt ngón tay lên cảm biến...");
+
+  //Đọc hình ảnh vân tay
+  int result = finger.getImage();
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Không thể đọc vân tay: ");
+    Serial.println(result);
+    return;
+  }
+
+  //Chuyển đổi hình ảnh thành dữ liệu vân tay
+  result = finger.image2Tz();
+  if (result != FINGERPRINT_OK) {
+    Serial.print("Lỗi khi tạo dữ liệu vân tay: ");
+    Serial.println(result);
+    return;
+  }
+
+  //Tìm kiếm vân tay trong bộ nhớ
+  result = finger.fingerSearch();
+  if (result == FINGERPRINT_OK) {
+    Serial.println("vân tay khớp!");
+    Serial.print("ID: ");
+    Serial.println(finger.fingerID); // 
+  } else if (result == FINGERPRINT_NOTFOUND) {
+    Serial.println("Không tìm thấy vân tay!");
+  } else {
+    Serial.print("Lỗi khi tìm vân tay: ");
+    Serial.println(result);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting...")
+  Serial.println("Booting...");
 
   if (init_wifi() != ESP_OK) {
-      Serial.println("WiFi initialization failed. Restarting...");
+      Serial.println("WiFi initialization failed!");
   }
-
+  initFinger();// Khởi tạo finger
+  nfc.begin(); // Khởi tạo RFID
 }
 
 void loop() {
+  client.poll();//giữ kết nối socket
   char key = keypad.getKey();//lấy ký tự từ bàn phím
-
   if (key) {
     Serial.println(key);
   }
+if (nfc.inListPassiveTarget()) { // Kiểm tra xem có thẻ nào không
+    // Đọc thông tin thẻ
+    uint8_t success;
+    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 }; // UID của thẻ
+    uint8_t uidLength; // Chiều dài UID
 
+    // Đọc UID từ thẻ
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+    if (success) {
+        Serial.print("UID: ");
+        for (uint8_t i=0; i < uidLength; i++) {
+          Serial.print(uid[i], HEX);
+          if (i < uidLength - 1) {
+              Serial.print(":");
+          }
+        }
+        Serial.println();
+
+        // Xử lý và lưu dữ liệu thẻ
+        String tag_id = "";
+        for (uint8_t i = 0; i < uidLength; i++) {
+            tag_id += String(uid[i], HEX);
+        }
+        // Lưu thời gian hiện tại
+        String timestamp = String(millis()); // Hoặc sử dụng thời gian thực
+      
+    }
+  }
 }
