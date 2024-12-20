a0e4f1ae81beaa5c0b45300ca053476760cd0071
vandungthieu <tvzung20102003@gmail.com>
2024-12-20 17:08:16 +0700

    fix model

--- backend/models/Device.js
+++ backend/models/Device.js
@@ -1,7 +1,8 @@
+const mongoose = require('mongoose');
+
 const deviceSchema = new mongoose.Schema({
-    id_port: { type: String, required: true, unique: true },
-    uid: { type: String, required: true, ref: 'User' }, // Tham chiếu đến User qua UID
-  });
-  
-  module.exports = mongoose.model('Device', deviceSchema);
-  
+  id_port: { type: String, required: true, unique: true },
+  UID: { type: mongoose.Schema.Types.String, required: true, ref: 'User' }, // Tham chiếu UID của User
+});
+
+module.exports = mongoose.model('Device', deviceSchema);
--- backend/models/History.js
+++ backend/models/History.js
@@ -1,10 +1,11 @@
+const mongoose = require('mongoose');
+
 const historySchema = new mongoose.Schema({
-    id_port: { type: String, required: true },
-    UID: { type: String, required: true, ref: 'User' }, // Tham chiếu đến User qua UID
-    time_in: { type: String, required: true },
-    time_out: { type: String, required: true },
-    status: { type: Boolean, required: true },
-  });
-  
-  module.exports = mongoose.model('History', historySchema);
-  
+  id_port: { type: String, required: true },
+  UID: { type: mongoose.Schema.Types.String, required: true, ref: 'User' }, // Tham chiếu UID của User
+  time_in: { type: String, required: true },
+  time_out: { type: String, required: true },
+  status: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('History', historySchema);
