38b938d773ad211243e715bb10c1e562496189bd
Lenhatduc <lenhatduc30042003@gmail.com>
2024-12-28 15:24:51 +0700

    Change 2

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -2,19 +2,12 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { NavLink, useNavigate } from 'react-router-dom'; // Import useNavigate ở đây
+import { NavLink } from 'react-router-dom'; // Import useNavigate ở đây
 import "./Sidebar.css";
 
 library.add(fas);
 
-function Sidebar({ onLogout }) {
-  const navigate = useNavigate(); // Khởi tạo useNavigate
-
-  const handleLogout = () => {
-    onLogout(); // Gọi hàm đăng xuất
-    navigate("/login"); // Chuyển hướng đến trang login
-  };
-
+function Sidebar({}) {
   return (
     <div className="sidebar">
       <NavLink className={({ isActive }) => (isActive ? "menu-item active" : "menu-item")} to='/access-management'>
@@ -36,11 +29,6 @@ function Sidebar({ onLogout }) {
         <FontAwesomeIcon icon="fa-solid fa-bars-progress" />
         <div className="sidebar-title">Quản lý người dùng</div>
       </NavLink>
-
-      <div className="menu-item" onClick={handleLogout}>
-        <FontAwesomeIcon icon="fa-solid fa-right-from-bracket" />
-        <div className="sidebar-title">Đăng xuất</div>
-      </div>
     </div>
   );
 }
